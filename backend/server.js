086d27009d7ae6ce86586150bdd416daae0b4ba1
LinBrox <97131958+LinBrox@users.noreply.github.com>
2023-03-23 17:42:40 +0000

    heroku changes

--- backend/server.js
+++ backend/server.js
@@ -6,15 +6,12 @@ const certRoutes = require('./routes/certRoutes');
 const {notFound, errorHandler} = require('./middlewares/errorMiddleware');
 const { isAdmin, protect } = require('./middlewares/authMiddleware');
 const User = require('./models/userModel');
+const path = require('path');
 
 const app = express();
 dotenv.config();
 connectDB();
 app.use(express.json());
-
-app.get('/',(req,res) =>{
-    res.send("API is running");
-});
 
 app.delete('/api/users/:id', protect, isAdmin, async (req, res) => {
   try {
@@ -47,6 +44,22 @@ app.get('/api/certTest', (req,res) => {
 
 app.use('/api/users', userRoutes);
 app.use('/api/certs', certRoutes);
+
+// Deployment
+__dirname = path.resolve()
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")))
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  })
+} else{
+  app.get('/',(req,res) =>{
+  res.send("API is running");
+});
+
+}
+
 
 app.use(notFound)
 app.use(errorHandler)
